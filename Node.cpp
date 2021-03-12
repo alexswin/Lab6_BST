d0fc1e632bd3f25ce2d34d168cc3089d9230f317
alexswin <alexswindler@gmail.com>
2021-03-12 03:45:29 +0000

    Initial commit

---
+++ Node.cpp
@@ -0,0 +1,27 @@
+#ifndef NODE_CPP
+#define NODE_CPP
+
+#include "Node.h"
+
+Node::Node() {
+
+}
+
+Node::~Node() {
+
+}
+
+int Node::getData() const {
+
+}
+
+Node* Node::getLeftChild() const {
+
+}
+
+Node* Node::getRightChild() const {
+
+}
+
+
+#endif
